26fda44d0229d3d2fa1b9d788e2a8d0b56e53a4a
LronDC <lun.su@daocloud.io>
2023-12-01 16:07:58 +0800

    optimize plugin controller workflow

--- internal/controller/scoringplugin_controller.go
+++ internal/controller/scoringplugin_controller.go
@@ -19,6 +19,7 @@ package controller
 import (
 	"context"
 	"encoding/json"
+	"math/rand"
 	"os"
 	"path/filepath"
 
@@ -146,9 +147,11 @@ func (r *ScoringPluginReconciler) applyYAML(ctx context.Context, path string, sc
 
 	// Convert the file content to a string
 	yamlStr := string(yamlFile)
-
+	// Generate a random string
+	randomString := r.generateRandomString(5) // You can customize the length of the random string
+	objName := scoring.GetName() + "-" + randomString
 	// Replace placeholders with environment variable values and run-time parameters defined in the dataset
-	replacedYamlStr, err := r.replacePlaceholders(yamlStr, parameters, scoring)
+	replacedYamlStr, err := r.replacePlaceholders(yamlStr, parameters, scoring, objName)
 	if err != nil {
 		r.Log.Errorf("unable to replace placeholders in YAML: %v", err)
 		return err
@@ -173,6 +176,9 @@ func (r *ScoringPluginReconciler) applyYAML(ctx context.Context, path string, sc
 		return err
 	}
 
+	// Modify the name of unstructuredObj
+	unstructuredObj.SetName(objName)
+
 	// Apply the unstructured object using the client
 	if err := r.applyClient(ctx, unstructuredObj); err != nil {
 		r.Log.Errorf("unable to apply Unstructured object: %v", err)
@@ -182,13 +188,24 @@ func (r *ScoringPluginReconciler) applyYAML(ctx context.Context, path string, sc
 	return nil
 }
 
+// generateRandomString generates a random string of specified length
+func (r *ScoringPluginReconciler) generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(result)
+}
+
 // replacePlaceholders replaces a specific placeholder in the YAML file with the value from an environment variable
-func (r *ScoringPluginReconciler) replacePlaceholders(yamlStr string, parameters map[string]interface{}, scoring *extensionv1beta1.Scoring) (string, error) {
+func (r *ScoringPluginReconciler) replacePlaceholders(yamlStr string, parameters map[string]interface{}, scoring *extensionv1beta1.Scoring, objName string) (string, error) {
 
 	// Add the required fields defined in the plugin standard to parameters
 	baseUrl := config.GetCompleteNotifyURL()
-	parameters["CompleteNotifyUrl"] = "http://patch-k8s-server." + config.GetDatatunerxSystemNamespace() + ".svc.cluster.local" + baseUrl + scoring.Namespace + "/scorings/" + scoring.Name
+	parameters["CompleteNotifyUrl"] = "http://patch-k8s-server." + config.GetDatatunerxSystemNamespace() + ".svc.cluster.local" + baseUrl + scoring.Namespace + "/scorings/" + scoring.Name + "/" + objName
 	parameters["InferenceService"] = scoring.Spec.InferenceService
+	parameters["Name"] = objName
 	r.Log.Infof("Replacing placeholder: %s", parameters)
 	// Replace the value in template yaml
 	replacedYamlStr, err := parser.ReplaceTemplate(yamlStr, parameters)
